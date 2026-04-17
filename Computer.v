0b53725e265dc5ec28e8d4d9cb02176810c306b8
OladayoOyedeji <robertoyedeji7@gmail.com>
2026-04-17 12:28:38 -0500

    multicycle

--- Computer.v
+++ Computer.v
@@ -1,80 +1,32 @@
-module twoscomplement(b, out, clock);
-   input [31:0]  b;
-   input         clock;
-   output [31:0] out;
-
-   reg [31:0]    out;
-
-   integer       i;
-   reg           carry;
-   always @ (posedge clock) begin
-      carry = 1;
-      for (i = 0; i < 32; i = i + 1) begin
-         out[i] = !b[i] ^ carry;
-         
-         carry = carry & !b[i];
-      end
-   end
-   
-endmodule // twoscomplement
-
-module adder(a, b, sum, clock);
-   input [31:0] a;
-   input [31:0] b;
-
-   input        clock;
-
-   output [31:0] sum;
-
-   reg [31:0]    sum;
-
-   integer       i;
-   reg           carry;
-
-   always @ (posedge clock) begin
-      carry = 0;
-      
-      for (i = 0; i < 32; i = i + 1) begin
-         sum[i] = (a[i] ^ b[i]) ^ carry;
-         carry = (a[i] & b[i]) | ((a[i] ^ b[i]) & carry);
-         // $monitor("carry=%b",
-         //          carry);
-      end
-   end
-endmodule // adder
-
-module mux(a,b,s,out,clock);
+module ALU(a, b, op, result, zero, clock);
    input [31:0]  a;
    input [31:0]  b;
-   input         s;
+   input [3:0]   op;
    input         clock;
-   output [31:0] out;
-
-   reg [31:0]    out;
-   integer       i;
-
-   always @ (posedge clock) begin
-      out = 0;
-      for (i = 0; i < 32; i = i + 1) begin
-         out[i] = (a[i] & !s) | (b[i] & s);
-      end
-   end
-endmodule // mux
-
-module smallALU(a, b, op, result, clock);
-   input [31:0]  a;
-   input [31:0]  b;
-   input         op;
-   input         clock;
-
    output [31:0] result;
-   wire [31:0]   twos_b;
-   wire [31:0]   sum;
-   wire [31:0]   rhs;
 
-   twoscomplement twos(.b(b), .out(twos_b), .clock(clock));
-   mux choice(.a(b), .b(twos_b), .s(op), .out(rhs), .clock(clock));
-   adder add1(.a(a), .b(rhs), .sum(result), .clock(clock));
+   always @(*) begin
+      case (op)
+        4'b0010: begin
+           result <= a + b;
+        end
+        4'b0110: begin
+           result <= a - b;
+        end
+        4'b0000: begin
+           result <= a & b;
+        end
+        4'b0001: begin
+           result <= a | b;
+        end
+        4'b0111: begin
+           result <= a < b;
+        end
+      endcase // case (op)
+   end // always @ (*)
+
+   assign zero = (result == 32'b0);
+   
 endmodule // smallALU
 
 module Decoder(code, decoded, clock);
@@ -97,9 +49,8 @@ module Decoder(code, decoded, clock);
    end // always @ (posedge clock)
 endmodule
 
-module Registerfile(read1, read2, write_register, 
-                    write_data, read_data_1,
-                    read_data_2, reg_write, clock);
+module Registerfile(read1, read2, write_register, write_data,
+                    read_data_1, read_data_2, reg_write, clock);
    input [4:0]   read1, read2, write_register;
 
    input [31:0]  write_data;
@@ -121,56 +72,55 @@ module Registerfile(read1, read2, write_register,
    end
 endmodule // Registerfile
 
-module Datapath(instruction, RegDst, RegWrite, writeData, data1, data2, clock);
-   input [31:0]  instruction;
-   input [31:0]  writeData;
+// module Datapath(instruction, RegDst, RegWrite, writeData, data1, data2, clock);
+//    input [31:0]  instruction;
+//    input [31:0]  writeData;
 
-   input         RegDst;
-   input         RegWrite;
-   input         clock;
-   output [31:0] data1;
-   output [31:0] data2;
+//    input         RegDst;
+//    input         RegWrite;
+//    input         clock;
+//    output [31:0] data1;
+//    output [31:0] data2;
    
    
-   wire [4:0]    readRegister1;
-   wire [4:0]    readRegister2;
-   wire [4:0]    readRegister3;
-   wire [4:0]    writeRegister;
+//    wire [4:0]    readRegister1;
+//    wire [4:0]    readRegister2;
+//    wire [4:0]    readRegister3;
+//    wire [4:0]    writeRegister;
 
-   assign readRegister1 = instruction[25:21]; // rs
-   assign readRegister2 = instruction[20:16]; // rt
-   assign readRegister3 = instruction[15:11]; // rd
+//    assign readRegister1 = instruction[25:21]; // rs
+//    assign readRegister2 = instruction[20:16]; // rt
+//    assign readRegister3 = instruction[15:11]; // rd
    
-   wire [4:0] writeReg = RegDst ? readRegister2: readRegister3;
+//    wire [4:0] writeReg = RegDst ? readRegister2: readRegister3;
    
-   // Mux mux(
-   //         .a(readRegister2), 
-   //         .b(instruction[15:11]), 
-   //         .s(RegDist),
-   //         .out(writeRegister),
-   //         .clock(clock)
-   //         );
+//    // Mux mux(
+//    //         .a(readRegister2), 
+//    //         .b(instruction[15:11]), 
+//    //         .s(RegDist),
+//    //         .out(writeRegister),
+//    //         .clock(clock)
+//    //         );
 
-   Registerfile registers(
-                    .read1(readRegister1),
-                    .read2(readRegister2),
-                    .write_register(writeRegister),
-                    .write_data(writeData),
-                    .read_data_1(data1),
-                    .read_data_2(data2),
-                    .reg_write(RegWrite),
-                    .clock(clock)
-                    );
-   
+//    Registerfile registers(
+//                     .read1(readRegister1),
+//                     .read2(readRegister2),
+//                     .write_register(writeRegister),
+//                     .write_data(writeData),
+//                     .read_data_1(data1),
+//                     .read_data_2(data2),
+//                     .reg_write(RegWrite),
+//                     .clock(clock)
+//                     );
 
-endmodule // control
+// endmodule // control
 
-module SRAM(address, dout, din, writeEnable, 
-            readEnable, clock);
+module SRAM(address, dout, din, MemWrite, 
+            MemRead, clock);
    input [31:0]      address;
    input [31:0]      din;          // Added from your port list
-   input             writeEnable;  // Added from your port list
-   input             readEnable;   // Added from your port list
+   input             MemWrite;  // Added from your port list
+   input             MemRead;   // Added from your port list
    input             clock;
    output reg [31:0] dout;      // Must be 'reg' because it's assigned in an always block
 
@@ -184,17 +134,17 @@ module SRAM(address, dout, din, writeEnable,
 
    // 2. Synchronous Read/Write Logic
    always @(posedge clock) begin
-      if (writeEnable) begin
+      if (MemWrite) begin
          memory[address[5:0]] <= din; // Use lower 6 bits for 64 entries
       end
       
-      if (readEnable) begin
+      if (MemRead) begin
          dout <= memory[address[5:0]];
       end
    end
 endmodule // SRAM
 
-module mem(PC, out, clock);
+module instruction_memory(PC, out, clock);
    input  [31:0] PC;
    input         clock; // Changed from int to input
    output [31:0] out;
@@ -206,16 +156,237 @@ module mem(PC, out, clock);
       .address({26'b0, word_index}),
       .dout(out),
       .din(32'b0),          // Instruction memory is usually read-only
-      .writeEnable(1'b0),   // Disable writing
-      .readEnable(1'b1),    // Always enable reading
+      .MemWrite(1'b0),   // Disable writing
+      .MemRead(1'b1),    // Always enable reading
       .clock(clock)
    );
 
 endmodule; // mem
 
-module Instruction_Pipeline(instruction, clock);
-   // instruction [25:0] -> control circuit
-   // instruction [25:21] -> register circuit as read register 1
-   // instruction [20:16] -> register circuit as read register 2
-   // (instruct[20:16], [15-11]) 0 for first 1 for 2nd
-   // instruction [15:0] -> register circuit as read register 2
+module Control(Opcode, PCWriteCond, PCWrite, IorD, MemRead, 
+               MemWrite, MemtoReg, IRWrite, PCSource, 
+               ALUOp, ALUSrcA, ALUSrcB, RegWrite, RegDst);
+   input [5:0]  Opcode;
+   output       ALUSrcA, MemRead, MemWrite, MemtoReg, IorD;
+   output       PCWrite, PCWriteCond, IRWrite, PCWrite, RegDst, RegWrite;
+   output [1:0] ALUOp, ALUSrcB, PCSource;
+   localparam   FETCH  = 4'd0, DECODE = 4'd1, MEM_ADR= 4'd2,  MEM_RD = 4'd3,
+                MEM_WB = 4'd4, EXEC   = 4'd6, R_TYPE = 4'd7, BEQ_STATE = 4'd8,
+                J_STATE = 4'd9;
+   
+   reg [3:0]    state, next_state;
+
+   // Sequential block to update the current state
+   always @(posedge clock or posedge reset) begin
+       if (reset) state <= FETCH;
+       else       state <= next_state;
+   end
+   
+   always @(*) begin
+        // Initialize all outputs to 0 to avoid unintended latches
+        {ALUSrcA, MemRead, MemWrite, MemtoReg, IorD} = 5'b0;
+        {PCWrite, PCWriteCond, IRWrite, RegDst, RegWrite} = 5'b0;
+        {ALUOp, ALUSrcB, PCSource} = 6'b0;
+        next_state = FETCH;
+      
+      case (state)
+        FETCH: begin
+           MemRead = 1;
+           IorD = 0;
+           IRWrite = 1;
+           ALUSrcB = 2'b01; // Constant 4
+           ALUSrcA = 0;     // Use PC
+           ALUOp = 2'b00;   // Add
+           PCWrite = 1;
+           PCSource = 2'b00;
+           next_state = DECODE;
+        end
+
+        DECODE: begin
+           ALUSrcA = 0;
+           ALUSrcB = 2'd3;
+           ALUOp = 2'b00;
+           RegWrite = 0;
+           
+           if (Opcode == 6'h23 || Opcode == 6'h2b)
+             next_state = MEM_ADR;
+           else if (Opcode == 6'h0)
+             next_state = EXEC;
+           else if (Opcode == 6'h4)
+             next_state = 4'd8;
+           else if (Opcode == 6'h2)
+             next_state = 4'd9;
+        end // case: DECODE
+
+        MEM_ADR: begin
+           ALUSrcB = 2'b10;
+           ALUSrcA = 1;
+           ALUOp = 2'b00;
+           
+           if (Opcode == 6'h23)
+             next_state = MEM_RD;
+           else
+             next_state = 4'd5;
+        end
+
+        MEM_RD: begin
+           IorD = 1;
+           MemRead = 1;
+           next_state = MEM_WB;
+        end
+
+        MEM_WB: begin
+           RegWrite = 1;
+           MemtoReg = 1;
+           RegDst = 0;
+           next_state = FETCH;
+        end
+
+        4'd5: begin
+           IorD = 1;
+           MemWrite = 1;
+           next_state = FETCH;
+        end
+
+        EXEC: begin
+           ALUSrcA = 1;
+           ALUSrcB = 2'b00;
+           ALUOp = 2'b10;
+           next_state = R_TYPE;
+        end
+
+        R_TYPE: begin
+           MemtoReg = 0;
+           RegWrite = 1;
+           RegDst = 1;
+           next_state = FETCH;
+        end
+
+        BEQ_STATE: begin
+           ALUOp = 2'b01;
+           ALUSrcA = 1;
+           ALUSrcB = 2'b00;
+           PCSource = 2'b01;
+           PCWriteCond = 1;
+           next_state = FETCH;
+        end
+
+        J_STATE: begin
+           PCSource = 2;
+           PCWrite = 1;
+           next_state = FETCH;
+        end
+      endcase
+endmodule // Control
+
+module ALUControl(ALUOp, Funct, ALUControlOut, clock);
+   input [1:0]  ALUOp;
+   input [5:0]  Funct;
+   output [3:0] ALUControlOut;
+   always @(*) begin
+      case (ALUOp)
+        2'b00: ALUControlOut = 4'b0010; // Force Add (LW, SW, ADDI)
+        2'b01: ALUControlOut = 4'b0110; // Force Subtract (BEQ)
+        
+        2'b10: begin // R-type: Look at Funct field
+           case (Funct)
+             6'h20: ALUControlOut = 4'b0010; // ADD
+             6'h22: ALUControlOut = 4'b0110; // SUB
+             6'h24: ALUControlOut = 4'b0000; // AND
+             6'h25: ALUControlOut = 4'b0001; // OR
+             6'h2a: ALUControlOut = 4'b0111; // SLT
+             default: ALUControlOut = 4'b0000;
+           endcase
+        end
+        
+        default: ALUControlOut = 4'b0010; // Default to Add
+      endcase
+   end
+endmodule // ALUControl
+
+module Multicycle_Pipeline;
+   reg [31:0]  PC;
+   reg [31:0]  Instruction;
+   reg [31:0]  MemoryData;
+
+   reg         IorD;
+   reg [31:0]  Address;
+   reg [31:0]  ALUOut;
+   reg [1:0]   ALUSrcB;
+   reg         MemWrite;
+   reg         MemRead;
+   reg         IRWrite;
+   reg         RegDst;
+   reg         RegWrite;
+   reg         MemtoReg;
+
+   wire [31:0] Address;
+   wire [31:0] Write_data;
+   wire [31:0] A;
+   wire [31:0] signextend;
+   
+   // PC to memory address or aluoutput
+   if ( (PCWriteCond & zero) | PCWrite)
+     PC = jumpaddress;
+   
+   assign Address = IorD ? ALUOut : PC;
+
+   // address used in memory data
+   SRAM memory(
+               .address(Address),
+               .dout(MemoryData),
+               .din(WriteData),
+               .MemWrite(MemWrite),
+               .MemRead(MemRead),
+               .clock(clock)
+               );
+
+   // instruction write
+   assign Instruction = IRWrite ? MemoryData : Instruction;
+   Control control(
+                   .Opcode(Instruction[31:26]), .PCWriteCond(PCWriteCond), .PCWrite(PCWrite),
+                   .IorD(IorD), .MemRead(MemRead), .MemWrite(MemWrite), .MemtoReg(MemtoReg),
+                   .IRWrite(IRWrite), .PCSource(PCSource), .ALUOp(ALUop),
+                   .ALUSrcA(ALUSrcA), .ALUSrcB(ALUSrcB), .RegWrite(RegWrite),
+                   .RegDst(RegDst)
+                   );
+   assign WriteData = MemtoReg ? MemoryData : ALUOut;
+   assign signextended = {{16{Instruction[15]}}, Instruction[15:0]};
+   assign WriteRegister = RegDst ? instruction[15:11] : instruction[20:16];
+   RegisterFile register(
+                         .read1(instruction[25:21]), .read2(instruction[20:16]),
+                         .write_register(WriteRegister), .write_data(WriteData),
+                         .read_data_1(ReadData1), .read_data_2(ReadData2),
+                         .reg_write(WriteData), .clock(clock)
+                         );
+   
+   assign A = ALUSrcA ? ReadData1 : PC;
+   always @(*) begin
+      case (ALUSrcB)
+        2'b00: B = ReadData2;          // R-type instructions
+        2'b01: B = 32'd4;             // PC + 4 increment
+        2'b10: B = signextended;      // I-type instructions (e.g., addi, lw)
+        2'b11: B = signextended << 2; // Branch offsets (PC-relative)
+        default: B = 32'b0;           // Safety catch-all
+      endcase // case (ALUSrcB)
+   end
+         
+   ALUControl alucontrol(
+                         .ALUOp(ALUOp), .Funct(Instruction[5:0]),
+                         .ALUControlOut(ALUControlOut), .clock(clock)
+                         );
+   ALU alu(
+           .a(A), .b(B), 
+           .op(ALUControlOut), 
+           .result(ALUresult), 
+           .zero(zero), .clock(clock)
+           );
+   
+   always @(*) begin
+      case (PCSource)
+        2'b00: jumpaddress = ALUresult;
+        2'b01: jumpaddress = ALUOut;
+        2'b10: jumpaddress = (Instruction[25:0] << 2) | (PC[31:28] << 28);
+      endcase
+   end
+endmodule // Multicycle_Pipeline
